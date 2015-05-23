ef386da46052d52806dc84866290c1c9f185652f
Sebastian <c0mpas@web.de>
2015-05-23 12:58:46 +0200

    implemented Sequence class

--- Bioinformatik/src/Sequence.java
+++ Bioinformatik/src/Sequence.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 
 public class Sequence {
 
@@ -12,18 +14,41 @@ public class Sequence {
 		this.seq = s;
 	}
 	
+	public String getSequence() {
+		return this.seq;
+	}
+	
 	// Erzeugt eine zufällige Sequenz der Länge n
-	public void generate(int n) {
-		
+	public String generate(int n) {
+		Random rand = new Random();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= n; i++) {
+			switch (rand.nextInt(4)) {
+				case 0: sb.append("A");
+						break;
+				case 1: sb.append("C");
+						break;
+				case 2: sb.append("G");
+						break;
+				case 3: sb.append("T");
+						break;
+				default: break;
+			}
+		}
+		return sb.toString();
 	}
 	
 	// Testet, ob die Sequenz ab der Stelle i gleich einer weiteren angegebenen Sequenz s ist
 	public boolean compare(Sequence s,int i) {
-		return true;
+		if (i < 0) throw new RuntimeException("index must be >= 0");
+		if (i > seq.length()-1) return false;
+		if (seq.substring(i, seq.length()-1) == s.getSequence()) return true;
+		return false;
 	}
 	
 	// Berechnet die maximale Überlappung vom Suffix der Sequenz mit dem Präfix der übergebenen Sequenz s
 	public int overlap(Sequence s) {
+		// todo
 		return 0;
 	}
 	
@@ -33,8 +58,8 @@ public class Sequence {
 			switch (seq.charAt(i)) {
 				case 'A': break;
 				case 'C': break;
-				case 'T': break;
 				case 'G': break;
+				case 'T': break;
 				default : return false;			
 			}
 		}
