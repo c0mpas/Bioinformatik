d7b9929f21b2736868df2e6146e10da2dcf876e4
cgeidt <cgeidt@web.de>
2015-05-23 13:44:26 +0200

    Some changes in class Node

--- Bioinformatik/src/Node.java
+++ Bioinformatik/src/Node.java
@@ -1,4 +1,56 @@
 
 public class Node {
 
+	private Edge edge;
+	private int overlap;
+	private Sequence sequence;
+	
+	public Node(){
+		
+	}
+	
+	public Node(Edge edge){
+		this(new Sequence(), -1);
+	}
+	
+	public Node(Sequence sequence, int overlap){
+		this();
+	}
+	
+	public Node(Sequence sequence, int overlap, Edge edge){
+		setEdge(edge);
+		setSequence(sequence);
+		setOverlap(overlap);
+	}
+	
+	public Node(Node startNode, Edge edge, Node endNode){
+
+	}
+
+	public Edge getEdge() {
+		return edge;
+	}
+
+	public void setEdge(Edge edge) {
+		this.edge = edge;
+	}
+
+	public int getOverlap() {
+		return overlap;
+	}
+
+	public void setOverlap(int overlap) {
+		this.overlap = overlap;
+	}
+
+	public Sequence getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(Sequence sequence) {
+		this.sequence = sequence;
+	}
+	
+	
+	
 }
