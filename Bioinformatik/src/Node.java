2e5c680642e3acd75305c4d2463fb75e8b818e9d
Sebastian <c0mpas@web.de>
2015-05-23 14:16:49 +0200

    added comments

--- Bioinformatik/src/Node.java
+++ Bioinformatik/src/Node.java
@@ -6,24 +6,29 @@ public class Node {
 	private ArrayList<Edge> edges;
 	private Sequence seq;
 	
+	// create new node with sequence
 	public Node(Sequence s) {
 		this.edges = new ArrayList<Edge>();
 		setSequence(s);
 	}
 	
+	// get sequence
 	public Sequence getSequence() {
 		return this.seq;
 	}
 
+	// set sequence
 	public void setSequence(Sequence s) {
 		if (s == null) throw new RuntimeException("sequence is null");
 		this.seq = s;
 	}
 
+	// get all outgoing edges
 	public ArrayList<Edge> getEdges() {
 		return this.edges;
 	}
 	
+	// add outgoing edge
 	public void addEdge(Edge edge) {
 		if (edge == null) throw new RuntimeException("edge is null");
 		edges.add(edge);
