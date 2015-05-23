020263c4c43720bb5266676e06e942af267edf92
cgeidt <cgeidt@web.de>
2015-05-23 15:03:11 +0200

    Creating graph

--- Bioinformatik/src/Graph.java
+++ Bioinformatik/src/Graph.java
@@ -9,6 +9,13 @@ public class Graph {
 		this.nodes = new ArrayList<Node>();
 	}
 	
+	public Graph(ArrayList<Sequence> sequences){
+		this();
+		for(Sequence sequence : sequences){
+			addNode(new Node(sequence));
+		}
+	}
+	
 	// add new node to graph
 	public void addNode(Node n) {
 		if (n==null) throw new RuntimeException("node is null");
@@ -19,10 +26,22 @@ public class Graph {
 	// integrate new node in graph by creating edges
 	private void connect(Node n) {
 		if (n==null) throw new RuntimeException("node is null");
-		// created edges for new node
-		// 1. edges from new node to other nodes
-		// 2. edges from other nodes to new node
-		// todo
+		
+		for(Node existingNode: nodes){
+			Edge edge;
+			int overlap;
+			overlap= n.getSequence().overlap(existingNode.getSequence()); 
+			if(overlap > 0){
+				edge = new Edge(n, existingNode);
+				n.addEdge(edge);
+			}
+			overlap = existingNode.getSequence().overlap(n.getSequence()); 
+			if(overlap > 0){
+				edge = new Edge(existingNode, n);
+				existingNode.addEdge(edge);
+			}
+		}
+		
 	}
 	
 	public void merge(Node a, Node b) {
