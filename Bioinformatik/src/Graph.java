41b60074d013c045c5df1ec489fadb5587388607
Sebastian <c0mpas@web.de>
2015-05-23 14:06:49 +0200

    .

--- Bioinformatik/src/Graph.java
+++ Bioinformatik/src/Graph.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
+
 public class Graph {
 
-	public Graph(){
-		
+	private ArrayList<Node> nodes;
+	
+	
+	public Graph() {
+		this.nodes = new ArrayList<Node>();
 	}
 	
-	public void sortEdgesDesc(){
-		
+	public void addNode(Node n) {
+		if (n == null) throw new RuntimeException("node is null");
+		this.nodes.add(n);
 	}
 	
 }
