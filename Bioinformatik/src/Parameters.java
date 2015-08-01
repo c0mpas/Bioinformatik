5aabed9cd19cdde354f38af8e9fb86b64865d186
Sebastian <c0mpas@web.de>
2015-08-01 19:01:42 +0200

    parameters class

--- Bioinformatik/src/Parameters.java
+++ Bioinformatik/src/Parameters.java
@@ -1,5 +1,20 @@
 
 public class Parameters {
 
-	public Parameters() {};
+	private static final int cap = 6;
+	
+	private String stateOne;
+	private String stateTwo;
+	private double[] pOne;
+	private double[] pTwo;
+	private double pSwitch;
+	
+	public Parameters() {
+		stateOne = null;
+		stateTwo = null;
+		pOne = new double[cap];
+		pTwo = new double[cap];
+		pSwitch = 0;
+	};
+	
 }
