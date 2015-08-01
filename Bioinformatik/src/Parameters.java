81cefeb36ee5a990fe90c9ed7cb2e5eb8bc26742
Sebastian <c0mpas@web.de>
2015-08-01 19:09:45 +0200

    implemented parameters class

--- Bioinformatik/src/Parameters.java
+++ Bioinformatik/src/Parameters.java
@@ -14,7 +14,54 @@ public class Parameters {
 		stateTwo = null;
 		pOne = new double[cap];
 		pTwo = new double[cap];
-		pSwitch = 0;
+		pSwitch = 0.0;
 	};
 	
+	public String getStateOne() {
+		return stateOne;
+	}
+
+	public void setStateOne(String stateOne) {
+		if (stateOne == null || stateOne.isEmpty()) throw new IllegalArgumentException();
+		this.stateOne = stateOne;
+	}
+
+	public String getStateTwo() {
+		return stateTwo;
+	}
+
+	public void setStateTwo(String stateTwo) {
+		if (stateTwo == null || stateTwo.isEmpty()) throw new IllegalArgumentException();
+		this.stateTwo = stateTwo;
+	}
+
+	public double getpOne(int i) {
+		if (i < 1 || i > 6) throw new IllegalArgumentException();
+		return pOne[i-1];
+	}
+
+	public void setpOne(int i, double p) {
+		if (i < 1 || i > 6 || p < 0.0) throw new IllegalArgumentException();
+		this.pOne[i-1] = p;
+	}
+
+	public double getpTwo(int i) {
+		if (i < 1 || i > 6) throw new IllegalArgumentException();
+		return pTwo[i-1];
+	}
+
+	public void setpTwo(int i, double p) {
+		if (i < 1 || i > 6 || p < 0.0) throw new IllegalArgumentException();
+		this.pTwo[i-1] = p;
+	}
+
+	public double getpSwitch() {
+		return pSwitch;
+	}
+
+	public void setpSwitch(double pSwitch) {
+		if (pSwitch < 0.0) throw new IllegalArgumentException();
+		this.pSwitch = pSwitch;
+	}
+
 }
